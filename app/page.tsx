364e1501c9fd83cbb5c4915421630e106bda89e2
Herlan Mustopa <hrmustopa.dev@gmail.com>
2025-08-01 20:05:30 +0700

    Change Style

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import Expertice from "@/components/templates/expertice";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    <Banner />
-    <Expertice />
-
+    <main>
+      <Banner />
+      <Expertice />
     </main>
   );
 }
