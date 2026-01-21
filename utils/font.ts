7b220ddae69fca75720ee3a121ba5d6ef5b5b49a
Herlan M. <hrmustopa.dev@gmail.com>
2026-01-22 02:05:51 +0700

    Refactor code structure for improved readability and maintainability

--- utils/font.ts
+++ utils/font.ts
@@ -1,8 +1,8 @@
-import {Albert_Sans, Unbounded} from "next/font/google";
+import { Albert_Sans, Unbounded } from "next/font/google";
 import localFont from "next/font/local";
 
 export const thesignature = localFont({
-  src: "../public/font/Thesignature.otf",
+  src: "./fonts/Thesignature.otf",
   display: "swap",
 });
 export const albert_Sans = Albert_Sans({
